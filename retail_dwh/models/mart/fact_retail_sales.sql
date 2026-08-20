a2b852bca30de160f041918b5b10e49b3e607523
“nhantran1104” <trandainhan.work@gmail.com>
2026-08-20 19:00:20 +0700

    refactor: code structure for improved readability and maintainability

--- retail_dwh/models/mart/fact_retail_sales.sql
+++ retail_dwh/models/mart/fact_retail_sales.sql
@@ -11,28 +11,25 @@ SELECT
         WHEN s.store_key = 0 THEN 'Online'
         ELSE 'In-store'
     END AS sales_channel,
+
     s.current_code,
     s.quantity,
     p.unit_price_usd,
     p.unit_cost_usd,
     s.quantity * p.unit_price_usd AS sales_amount_usd,
     s.quantity * p.unit_cost_usd AS cost_amount_usd,
-    (
-        s.quantity * p.unit_price_usd
-    ) - (
-        s.quantity * p.unit_cost_usd
-    ) AS profit_amount_usd,
+    (s.quantity * p.unit_price_usd) - (s.quantity * p.unit_cost_usd) AS profit_amount_usd,
+
     CASE
-        WHEN s.delivery_date IS NOT NULL THEN 1
-        ELSE 0
-    END AS is_delivered,
-    DATEDIFF(
-        'day',
-        s.order_date,
-        s.delivery_date
-    ) AS delivery_days
+        WHEN s.store_key = 0 AND s.delivery_date IS NOT NULL
+        THEN DATEDIFF(
+            'day',
+            s.order_date,
+            s.delivery_date
+        )
+        ELSE NULL
+    END AS delivery_days
 
 FROM {{ ref('stg_retail_line_items') }} s
-
 LEFT JOIN {{ ref('stg_retail_products') }} p
     ON s.product_key = p.product_key
